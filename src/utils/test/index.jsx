369529d654763a4433456b9437b9561075c02622
Philippe Sendze <sendze.philippe@gmail.com>
2023-01-11 10:16:51 +0100

    tests (jest and rtl) on theme actions and reducers

--- src/utils/test/index.jsx
+++ src/utils/test/index.jsx
@@ -1,14 +1,30 @@
 import { render as rtlRender } from '@testing-library/react'
-import { ThemeProvider, SurveyProvider } from '../../utils/context'
 import { MemoryRouter } from 'react-router-dom'
+import themeReducer from '../../features/theme'
+import freelancesReducer from '../../features/freelances'
+import surveyReducer from '../../features/survey'
+import freelanceReducer from '../../features/freelance'
+import resultsReducer from '../../features/results'
+import answersReducer from '../../features/answers'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
 
 export function render(ui, options) {
+  const store = configureStore({
+    reducer: {
+      theme: themeReducer,
+      freelances: freelancesReducer,
+      survey: surveyReducer,
+      freelance: freelanceReducer,
+      results: resultsReducer,
+      answers: answersReducer,
+    },
+  })
+
   function Wrapper({ children }) {
     return (
       <MemoryRouter {...options}>
-        <ThemeProvider>
-          <SurveyProvider>{children}</SurveyProvider>
-        </ThemeProvider>
+        <Provider store={store}>{children}</Provider>
       </MemoryRouter>
     )
   }
